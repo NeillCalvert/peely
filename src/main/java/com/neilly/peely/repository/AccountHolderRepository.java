69409d5f2ed1bd9c7d4fcb878b2ccbe797c056c4
Neill <neillcalvert@btinternet.com>
2022-04-23 17:02:48 +0100

    Pushing change to add model mapper

--- src/main/java/com/neilly/peely/repository/AccountHolderRepository.java
+++ src/main/java/com/neilly/peely/repository/AccountHolderRepository.java
@@ -4,13 +4,11 @@
 package com.neilly.peely.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.neilly.peely.model.AccountHolder;
-import com.neilly.peely.model.AccountHolderDTO;
 
 /**
  * @author neill
@@ -25,5 +23,5 @@ public interface AccountHolderRepository extends CrudRepository<AccountHolder, L
 	List<AccountHolder> findAllAccountHolders();
 	
 	AccountHolder findByEmail(String email);
-	
+
 }
