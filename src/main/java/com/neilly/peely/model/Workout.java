9ce6060c2c70d060641c2e6b3b91e28393ccd276
Neill <neillcalvert@btinternet.com>
2022-05-16 21:31:57 +0100

    Adding Workout model, service and controller classes

--- src/main/java/com/neilly/peely/model/Workout.java
+++ src/main/java/com/neilly/peely/model/Workout.java
@@ -1,2 +1,59 @@
-package com.neilly.peely.model;public class Workout {
+package com.neilly.peely.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Date date;
+    @ManyToOne()
+    @JoinColumn(name="accountHolder_id")
+    private AccountHolder accountHolder;
+
+    public Workout(){
+    }
+
+    public Workout(Long id, String name, Date date, AccountHolder accountHolder){
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.accountHolder = accountHolder;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public AccountHolder getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(AccountHolder accountHolder) {
+        this.accountHolder = accountHolder;
+    }
 }
--- src/main/java/com/neilly/peely/repository/WorkoutRepository.java
+++ src/main/java/com/neilly/peely/repository/WorkoutRepository.java
@@ -1,2 +1,15 @@
-package com.neilly.peely.repository;public interface WorkoutRepository {
+package com.neilly.peely.repository;
+
+import com.neilly.peely.model.Workout;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public interface WorkoutRepository extends CrudRepository<Workout, Long> {
+
+    List<Workout> findByAccountHolder_Username(String username);
+
+    List<Workout> findByDateAndAccountHolderUsername(Date date, String username);
+
 }
